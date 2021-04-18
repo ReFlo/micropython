7be534acdbed606f62a0007f16e14214edb19a04
Andreas Kirmeier <andreas.kirmeier@st.oth-regensburg.de>
2021-04-18 09:31:29 +0200

    mods/timer.h: added Timer register struct

--- ports/tm4c123/mods/timer.h
+++ ports/tm4c123/mods/timer.h
@@ -23,20 +23,60 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-#ifndef MICROPY_INCLUDED_STM32_TIMER_H
-#define MICROPY_INCLUDED_STM32_TIMER_H
+#ifndef MICROPY_INCLUDED_TM4C_TIMER_H
+#define MICROPY_INCLUDED_TM4C_TIMER_H
+
+#include <stdint.h>
+#include "driverlib/timer.h"
+#include "inc/hw_timer.h"
+
+/**
+ *  Timer register struct for easy access 
+ *  for register description, see datasheet
+*/
+typedef struct  {
+    volatile uint32_t CFG;
+    volatile uint32_t TAMR;
+    volatile uint32_t TBMR;
+    volatile uint32_t CTL;
+    volatile uint32_t SYNC;
+    uint32_t _1[1];
+    volatile uint32_t IMR;
+    volatile uint32_t RIS;
+    volatile uint32_t MIS;
+    volatile uint32_t ICR;
+    volatile uint32_t TAILR;
+    volatile uint32_t TBILR;
+    volatile uint32_t TAMATCHR;
+    volatile uint32_t TBMATCHR;
+    volatile uint32_t TAPR;
+    volatile uint32_t TBPR;
+    volatile uint32_t TAPMR;
+    volatile uint32_t TBPMR;
+    volatile uint32_t TAR;
+    volatile uint32_t TBR;
+    volatile uint32_t TAV;
+    volatile uint32_t TBV;
+    volatile uint32_t RTCPD;
+    volatile uint32_t TAPS;
+    volatile uint32_t TBPS;
+    volatile uint32_t TAPV;
+    volatile uint32_t TBPV;
+    uint32_t _2[981]; 
+    volatile uint32_t MPP;
+} periph_timer_t;
 
 //extern TIM_HandleTypeDef TIM5_Handle;
 
 extern const mp_obj_type_t pyb_timer_type;
 
-void timer_init0(void);
-void timer_tim5_init(void);
+//void timer_init0(void);
+//void timer_tim5_init(void);
 //TIM_HandleTypeDef *timer_tim6_init(uint freq);
-void timer_deinit(void);
-uint32_t timer_get_source_freq(uint32_t tim_id);
-void timer_irq_handler(uint tim_id);
+//void timer_deinit(void);
+//uint32_t timer_get_source_freq(uint32_t tim_id);
+//void timer_irq_handler(uint tim_id);
 
 //TIM_HandleTypeDef *pyb_timer_get_handle(mp_obj_t timer);
 
-#endif // MICROPY_INCLUDED_STM32_TIMER_H
+#endif // MICROPY_INCLUDED_TM4C_TIMER_H
